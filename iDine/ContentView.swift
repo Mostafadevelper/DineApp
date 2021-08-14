ce8c2e80df4fa7de6f681924806fd68df1d64675
millensys <mi>
2021-08-14 17:32:46 +0200

    add: list with navigation view in SwiftUI

---
+++ iDine/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Mostafa on 14/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Section(header:  Text(section.name).bold().foregroundColor(.red)) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(GroupedListStyle())
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Mostafa on 14/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
