5a99bcc9c431be93dd1d588d61b3c45e28911719
millensys <mi>
2021-08-15 17:23:25 +0200

    add: make design for fonts, colors and Images Circle

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -10,15 +10,29 @@ import SwiftUI
 struct ItemRow: View {
     
     let item: MenuItem
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
     
     var body: some View {
-        HStack {
-            Image(item.thumbnailImage)
-            VStack(alignment: .leading) {
-                Text(item.name)
-                Text("$\(item.price)")
+            HStack() {
+                Image(item.thumbnailImage)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.gray, lineWidth: 2.0))
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }
+                Spacer()
+                ForEach(item.restrictions, id: (\.self)) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(colors[restriction, default: .black])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
             }
-        }
     }
 }
 
