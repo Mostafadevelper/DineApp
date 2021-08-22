3177096c85a99df757e0d0211d7ba1c8cd9c209d
millensys <mi>
2021-08-22 11:29:47 +0200

    add: TabView and tabItem

---
+++ iDine/MainView.swift
@@ -0,0 +1,34 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Mostafa on 20/08/2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            
+            ContentView()
+                .tabItem {
+                    Text("Menu")
+                    Image(systemName: "list.dash")
+                }
+
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+                
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+            .environmentObject(Order())
+    }
+}
